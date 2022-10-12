28d265daa77e7ee8eb7b961a1e3b83c559933733
Ewout Stortenbeker <me@appy.one>
2022-10-12 11:31:21 +0200

    Handle errors without response (#3)
    * Handle errors when there's no response
    
    * Generated code

--- src/classes/ProfitError.ts
+++ src/classes/ProfitError.ts
@@ -2,18 +2,27 @@ export class ProfitError extends Error {
   public statusCode: number;
   public statusText: string;
   public body: any;
-  constructor (description: string, error:{ body: string, response: any }) {
-    let body;
-    try {
-      body = JSON.parse(error.body)
-    } catch (err) {
-      body = error.body
+  constructor (description: string, error: Error | { body: any, response: any }) {
+    if (!(error as any).response) {
+      console.warn(`Creating ProfitError without response:`, error);
     }
-    super(body ? body.externalMessage : description)
-    this.name = this.constructor.name
+    const body = (() => {
+      if (error instanceof Error) { return null; }
+      else if (typeof error?.body === 'object') { return error.body; }
+      else if (typeof error?.body === 'string') {
+        try {
+          return JSON.parse(error.body); 
+        }
+        catch (err) {
+          return error.body;
+        }
+      }
+    })();
+    super(body?.externalMessage ?? (error as Error).message ?? description);
+    this.name = this.constructor.name;
     Error.captureStackTrace(this, ProfitError);
-    this.statusCode = error.response.status
-    this.statusText = error.response.statusText
-    this.body = body
+    this.statusCode = (error as any).response?.status ?? 0;
+    this.statusText = (error as any).response?.statusText ?? '';
+    this.body = body;
   }
 }
