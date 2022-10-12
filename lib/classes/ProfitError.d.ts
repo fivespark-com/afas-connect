8f416d4874584dfcd956d86fb75cef801bf962a6
Ewout Stortenbeker <me@appy.one>
2022-10-12 11:12:56 +0200

    Generated code

--- lib/classes/ProfitError.d.ts
+++ lib/classes/ProfitError.d.ts
@@ -2,8 +2,8 @@ export declare class ProfitError extends Error {
     statusCode: number;
     statusText: string;
     body: any;
-    constructor(description: string, error: {
-        body: string;
+    constructor(description: string, error: Error | {
+        body: any;
         response: any;
     });
 }
